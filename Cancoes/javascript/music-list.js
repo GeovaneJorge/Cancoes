a4d1534ae993fbcdbe9b15adb44bd0944bd41761
Geovane <33160858+GeovaneJorge@users.noreply.github.com>
2024-07-02 22:38:51 -0300

    Canções DIRAP
    Canções DIRAP

---
+++ Cancoes/javascript/music-list.js
@@ -0,0 +1,21 @@
+let allMusic = [
+    {
+        name: "Canção da DIRAP",
+        artist: "Cel Int Danilo MALHEIROS Menezes",
+        img: "DIRAP",
+        src: "DIRAP"
+    },
+    {
+        name: "Canção do Especialista",
+        artist: "Especialista",
+        img: "Especialista",
+        src: "Especialista"
+    },
+    {
+        name: "Aviadores FAB",
+        artist: "Aviadores",
+        img: "Aviadores",
+        src: "Aviadores"
+    },
+   
+]
